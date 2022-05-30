3f60b0a6419ba54dfdeb1008930e9444db582375
Rahul6009 <106512278+Rahul6009@users.noreply.github.com>
2022-05-30 13:27:50 +0530

    One.java  file added

---
+++ one.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+class One 
+{
+  public static viod main(String[] args)
+  {
+    System.out.println("Welcome To Git Operations....");
+     System.out.println("Welcome To Git Operations....");
+     System.out.println("Welcome To Git Operations....");
+  }
+}
